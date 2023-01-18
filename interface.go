ab07d378dab3785bd4fd297e3dcdcc7e8dc74cbb
Karl Gaissmaier <karl.gaissmaier@uni-ulm.de>
2023-01-18 22:19:30 +0100

    doc

--- interface.go
+++ interface.go
@@ -46,7 +46,7 @@ package interval
 //  -------------------------|---------------------------------------|
 //  |          A1---A2       |  1 |  1 |  1 |  1 | A preceded by B   |
 //  |  B1---B2               |    |    |    |    |                   |
-//  -------------------------|---------------------------------------|
+//  -------------------------|----------------------------------------
 //
 // [Allen's Interval Algebra]: https://www.ics.uci.edu/~alspaugh/cls/shr/allen.html
 //
@@ -64,27 +64,29 @@ type Interface[T any] interface {
 }
 
 // compare is for sorting keys into the BST, the sort key is the left point of the intervals.
-// If the left point is equal, sort the supersets to the left.
+// If the left point is equal, sort the supersets to the left (definite order).
 //
-//  e.g. all relations with ll == 0
+//  e.g. special treatment for all relations with ll == 0
 //  =================================================================|
 //  |  visualization         | ll | rr | lr | rl | description       |
 //  =================================================================|
-//  |  A1---A2               |  0 | -1 | -1 |  1 | A starts B        | => sort interval A to the right
+//  ~                                                                ~
+//  -------------------------|---------------------------------------|
+//  |  A1---A2               |  0 | -1 | -1 |  1 | A starts B        | => A is covered by B, => sort A to the right
 //  |  B1-------B2           |    |    |    |    |                   |
 //  -------------------------|---------------------------------------|
 //  |  A1----A2              |  0 |  0 | -1 |  1 | A equals B        | => equality
 //  |  B1----B2              |    |    |    |    |                   |
 //  -------------------------|---------------------------------------|
-//  |  A1--------A2          |  0 |  1 | -1 |  1 | A started by B    | => sort interval A to the left
+//  |  A1--------A2          |  0 |  1 | -1 |  1 | A started by B    | => A covers B, => sort A to the left
 //  |  B1----B2              |    |    |    |    |                   |
-//  -------------------------|---------------------------------------|
+//  -------------------------|----------------------------------------
 //
 func compare[T Interface[T]](a, b T) int {
 	ll, rr, _, _ := a.Compare(b)
 	switch {
 	case ll == 0:
-		// identical left point, sort supersets to the left
+		// identical left point, sort supersets to the left, make a definite order
 		// if rr == 0, -rr makes no difference
 		return -rr
 	default:
@@ -93,23 +95,13 @@ func compare[T Interface[T]](a, b T) int {
 	}
 }
 
-// cmpRR, compares just the upper right point of the intervals.
-func cmpRR[T Interface[T]](a, b T) int {
-	_, rr, _, _ := a.Compare(b)
-	return rr
-}
-
-// cmpLR, compares just the left point from a with right point from b.
-func cmpLR[T Interface[T]](a, b T) int {
-	_, _, lr, _ := a.Compare(b)
-	return lr
-}
-
 // covers, returns true if a covers b.
 //
 //  =================================================================|
 //  |  visualization         | ll | rr | lr | rl | description       |
 //  =================================================================|
+//  ~                                                                ~
+//  -------------------------|---------------------------------------|
 //  |  A1------A2            | -1 |  0 | -1 |  1 | A finished by B   |
 //  |     B1---B2            |    |    |    |    |                   |
 //  -------------------------|---------------------------------------|
@@ -121,26 +113,40 @@ func cmpLR[T Interface[T]](a, b T) int {
 //  -------------------------|---------------------------------------|
 //  |  A1----A2              |  0 |  0 | -1 |  1 | A equals B        |
 //  |  B1----B2              |    |    |    |    |                   |
-//  -------------------------|---------------------------------------|
+//  -------------------------|----------------------------------------
 //
 func covers[T Interface[T]](a, b T) bool {
 	ll, rr, _, _ := a.Compare(b)
 	return ll <= 0 && rr >= 0
 }
 
-// intersects, returns true if the intervals does not precede each other.
+// intersects, returns false if the intervals does precede each other.
 //
 //  =================================================================|
 //  |  visualization         | ll | rr | lr | rl | description       |
 //  =================================================================|
-//  |  A1---A2               | -1 | -1 | -1 | -1 | A precedes B      |
+//  ~                                                                ~
+//  -------------------------|---------------------------------------|
+//  |  A1---A2               | -1 | -1 | -1 | -1 | A precedes B      | => false
 //  |           B1---B2      |    |    |    |    |                   |
 //  -------------------------|---------------------------------------|
-//  |          A1---A2       |  1 |  1 |  1 |  1 | A preceded by B   |
+//  |          A1---A2       |  1 |  1 |  1 |  1 | A preceded by B   | => false
 //  |  B1---B2               |    |    |    |    |                   |
 //  -------------------------|---------------------------------------|
 //
 func intersects[T Interface[T]](a, b T) bool {
 	ll, rr, lr, rl := a.Compare(b)
 	return !((ll == -1 && rr == -1 && lr == -1 && rl == -1) || (ll == 1 && rr == 1 && lr == 1 && rl == 1))
+}
+
+// cmpRR, compares just the right point of the intervals.
+func cmpRR[T Interface[T]](a, b T) int {
+	_, rr, _, _ := a.Compare(b)
+	return rr
+}
+
+// cmpLR, compares just the left point from a with right point from b.
+func cmpLR[T Interface[T]](a, b T) int {
+	_, _, lr, _ := a.Compare(b)
+	return lr
 }
